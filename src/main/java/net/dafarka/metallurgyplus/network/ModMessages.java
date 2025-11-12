10a43cee7c14421d039068b24d552b2443fd292b
Markus Farka <127229400+daFARKA@users.noreply.github.com>
2025-11-12 22:45:13 +0100

    cleaned packets

--- src/main/java/net/dafarka/metallurgyplus/network/ModMessages.java
+++ src/main/java/net/dafarka/metallurgyplus/network/ModMessages.java
@@ -2,12 +2,9 @@ package net.dafarka.metallurgyplus.network;
 
 import net.dafarka.metallurgyplus.MetallurgyPlus;
 import net.dafarka.metallurgyplus.network.packets.SetAreaQuarryPacket;
-import net.dafarka.metallurgyplus.network.packets.StartQuarryPacket;
-import net.dafarka.metallurgyplus.network.packets.StopQuarryPacket;
 import net.dafarka.metallurgyplus.network.packets.ToggleQuarryPacket;
 import net.minecraftforge.network.NetworkRegistry;
 import net.minecraftforge.network.simple.SimpleChannel;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraft.resources.ResourceLocation;
 
 public class ModMessages {
@@ -30,18 +27,6 @@ public class ModMessages {
             .encoder(SetAreaQuarryPacket::toBytes)
             .decoder(SetAreaQuarryPacket::new)
             .consumerMainThread(SetAreaQuarryPacket::handle)
-            .add();
-
-        net.messageBuilder(StartQuarryPacket.class, id())
-            .encoder(StartQuarryPacket::toBytes)
-            .decoder(StartQuarryPacket::new)
-            .consumerMainThread(StartQuarryPacket::handle)
-            .add();
-
-        net.messageBuilder(StopQuarryPacket.class, id())
-            .encoder(StopQuarryPacket::toBytes)
-            .decoder(StopQuarryPacket::new)
-            .consumerMainThread(StopQuarryPacket::handle)
             .add();
 
         net.messageBuilder(ToggleQuarryPacket.class, id())
