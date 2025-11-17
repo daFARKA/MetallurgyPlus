0dcaa8d17c942221a1ed583fd402cc9f7e5e705f
Markus Farka <127229400+daFARKA@users.noreply.github.com>
2025-11-17 12:50:41 +0100

    added forge tags to items

--- src/main/java/net/dafarka/metallurgyplus/datagen/ModItemTagGenerator.java
+++ src/main/java/net/dafarka/metallurgyplus/datagen/ModItemTagGenerator.java
@@ -6,11 +6,17 @@ import net.dafarka.metallurgyplus.util.ModTags;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.tags.ItemTagsProvider;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.ItemTags;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.common.data.ForgeItemTagsProvider;
+import net.minecraftforge.registries.RegistryObject;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
 public class ModItemTagGenerator extends ItemTagsProvider {
@@ -24,5 +30,24 @@ public class ModItemTagGenerator extends ItemTagsProvider {
     protected void addTags(HolderLookup.Provider pProvider) {
         this.tag(ItemTags.PICKAXES).add(ModItems.LLAMKANA.get());
 
+        addForgeTags(ModItems.MATERIAL_MAP);
+        addForgeTags(ModItems.ORE_MAP);
+        addForgeTags(ModItems.ALLOY_MAP);
+    }
+
+    private void addForgeTags(Map<String, RegistryObject<Item>> itemMap) {
+        for (RegistryObject<Item> item : itemMap.values()) {
+            addForgeTag(item);
+        }
+    }
+
+    private void addForgeTag(RegistryObject<Item> item) {
+        String name = item.getId().getPath();
+        TagKey<Item> tag = TagKey.create(
+            net.minecraft.core.registries.Registries.ITEM,
+            new ResourceLocation("forge", name)
+        );
+
+        this.tag(tag).add(item.get());
     }
 }
